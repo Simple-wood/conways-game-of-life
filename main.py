91e03522f5bb36e669abb17eb704444ebcab1305
Kishal <kishalc_06@outlook.com>
2026-09-11 19:46:55 +0100

    minor changes

--- main.py
+++ main.py
@@ -2,8 +2,10 @@ import pygame
 from Display import Simulation
 
 SCREEN_DIMENSIONS = (640, 480)
-CELL_SIZE = 32
+CELL_SIZES = (16, 20, 32, 40, 80, 160)
+SPEEDS = (1, 2, 3, 5, 6, 10, 15, 30)
 FONT_SIZE = 16
+FRAME_RATE = 30
 
 pygame.init()
 display = pygame.display.set_mode(SCREEN_DIMENSIONS)
@@ -12,7 +14,8 @@ clock = pygame.time.Clock()
 font = pygame.font.SysFont("ocraextended", FONT_SIZE, True)
 
 def main():
-    simulation_display = Simulation(display, font, SCREEN_DIMENSIONS[0], SCREEN_DIMENSIONS[1], CELL_SIZE)
+    simulation_display = Simulation(display, font, SCREEN_DIMENSIONS[0], SCREEN_DIMENSIONS[1], CELL_SIZES, len(CELL_SIZES), 2, SPEEDS, 
+                                    len(SPEEDS), 5, FRAME_RATE)
     running = True
     frame_count = 0
 
@@ -22,7 +25,7 @@ def main():
         running = simulation_display.loop(events, frame_count)
                     
         pygame.display.update()
-        clock.tick(30)
+        clock.tick(FRAME_RATE)
 
     pygame.quit()
 
